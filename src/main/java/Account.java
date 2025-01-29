deff710813c4962ad3190f1ae4cf500181268b69
Juan <juan.burillo@qaracter.com>
2025-01-29 10:10:19 +0000

    feat: add transfer method to Account class

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -34,14 +34,14 @@ public class Account {
      * Deposits an amount of money of the specified currency into the bank account
      * @param amount The amount of money to be deposited into the account
      * @param currency The currency
-     * @throws IllegalArgumentException Exception thrown if the amount to be deposited is negative or 0
      */
-    public void deposit(double amount, Currency currency) throws IllegalArgumentException {
-        if (amount >= 0) { // Checks if the amount is positive
+    public void deposit(double amount, Currency currency) {
+        try {
+            amountIsPositive(amount); // Checks if the amount is positive
             this.balance += amount;
             System.out.println("Successful deposit of " + amount + currency.toString());
-        } else {
-            throw new IllegalArgumentException("Deposit amount must be greater than 0.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -49,19 +49,48 @@ public class Account {
      * Withdraws an amount of money of the specified currency from the bank account
      * @param amount The amount of money to be withdrawn
      * @param currency The currency
-     * @throws InsufficientFundsException Exception thrown if the amount to be withdrawn is greater than the account balance
-     * @throws IllegalArgumentException Exception thrown if the amount to be withdrawn is negative or 0
      */
-    public void withdraw(double amount, Currency currency) throws InsufficientFundsException, IllegalArgumentException {
-        if (amount >= 0) {
-            if (amount <= this.balance) {
-                this.balance -= amount;
-                System.out.println("Successful withdrawal of " + amount + currency.toString());
-            } else {
-                throw new InsufficientFundsException("The operation cannot be completed because of insufficient funds.");
-            }
-        } else {
-            throw new IllegalArgumentException("Withdrawal amount must be greater than 0.");
+    public void withdraw(double amount, Currency currency) {
+        try {
+            amountIsPositive(amount); // Check if the amount is positive
+            hasSufficientFunds(amount); // Check if the account has sufficient funds to operate
+            this.balance -= amount;
+            System.out.println("Successful withdrawal of " + amount + currency.toString());
+        } catch (IllegalArgumentException | InsufficientFundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void transfer(double amount, Currency currency, Account account) {
+        try {
+            amountIsPositive(amount); // Check if the amount is positive
+            hasSufficientFunds(amount); // Check if the account has sufficient funds to operate
+            this.balance -= amount;
+            account.deposit(amount, currency);
+        } catch (IllegalArgumentException | InsufficientFundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Checks if the amount is positive and different from 0
+     * @param amount The amount to be validated
+     * @throws IllegalArgumentException Exception thrown if the amount is negative or 0
+     */
+    public void amountIsPositive(double amount) throws IllegalArgumentException {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0.");
+        }
+    }
+
+    /**
+     * Checks if th account has sufficient funds to perform an operation
+     * @param amount The amount to be validated
+     * @throws InsufficientFundsException Exception thrown if the amount is greater than the account balance
+     */
+    public void hasSufficientFunds(double amount) throws InsufficientFundsException {
+        if (amount > balance) {
+            throw new InsufficientFundsException("The operation cannot be completed because of insufficient funds.");
         }
     }
 
