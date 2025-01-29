fd64fc23c0c20a6d19c6b19e38e0280a9e9c8b98
Juan <juan.burillo@qaracter.com>
2025-01-29 09:41:08 +0000

    feat: create withdraw method in Account class

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -38,14 +38,34 @@ public class Account {
      * Deposits an amount of money of the specified currency into the bank account
      * @param amount The amount of money to be deposited into the account
      * @param currency The currency
-     * @throws IllegalArgumentException Exception thrown if the user inputs a negative value or 0
+     * @throws IllegalArgumentException Exception thrown if the amount to be deposited is negative or 0
      */
     public void deposit(double amount, Currency currency) throws IllegalArgumentException {
-        if (amount >= 0) {
+        if (amount >= 0) { // Checks if the amount is positive
             this.balance += amount;
             System.out.println("Successful deposit of " + amount + currency.toString());
         } else {
             throw new IllegalArgumentException("Deposit amount must be greater than 0.");
+        }
+    }
+
+    /**
+     * Withdraws an amount of money of the specified currency from the bank account
+     * @param amount The amount of money to be withdrawn
+     * @param currency The currency
+     * @throws InsufficientFundsException Exception thrown if the amount to be withdrawn is greater than the account balance
+     * @throws IllegalArgumentException Exception thrown if the amount to be withdrawn is negative or 0
+     */
+    public void withdraw(double amount, Currency currency) throws InsufficientFundsException, IllegalArgumentException {
+        if (amount >= 0) {
+            if (amount <= this.balance) {
+                this.balance -= amount;
+                System.out.println("Successful withdrawal of " + amount + currency.toString());
+            } else {
+                throw new InsufficientFundsException("The operation cannot be completed because of insufficient funds.");
+            }
+        } else {
+            throw new IllegalArgumentException("Withdrawal amount must be greater than 0.");
         }
     }
 
